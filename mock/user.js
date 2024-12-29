eaff618173bd7500e92b9485b83fa8c82196f492
leesh9098 <leesh9098@naver.com>
2024-12-29 22:09:24 +0900

    24.12.29.1 edit team list page, add team list mock data

---
+++ mock/user.js
@@ -0,0 +1,50 @@
+export const user1 = {
+    id: "user1@email.com",
+    name: "사용자 1"
+};
+
+export const user2 = {
+    id: "user2@email.com",
+    name: "사용자 2"
+};
+
+export const user3 = {
+    id: "user3@email.com",
+    name: "사용자 3"
+};
+
+export const user4 = {
+    id: "user4@email.com",
+    name: "사용자 4"
+};
+
+export const user5 = {
+    id: "user5@email.com",
+    name: "사용자 5"
+};
+
+export const user6 = {
+    id: "user6@email.com",
+    name: "사용자 6"
+};
+
+export const user7 = {
+    id: "user7@email.com",
+    name: "사용자 7"
+};
+
+export const user8 = {
+    id: "user8@email.com",
+    name: "사용자 8"
+};
+
+export const users = [
+    user1,
+    user2,
+    user3,
+    user4,
+    user5,
+    user6,
+    user7,
+    user8
+]
