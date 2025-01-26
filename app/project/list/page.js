2c63b4053b65545e294ad527a0679025e8a6d561
LimHaeJun <6462jun@gmail.com>
2025-01-26 21:52:09 +0900

    프로젝트 관리 페이지 수정, 수정 페이지 추가

--- app/project/list/page.js
+++ app/project/list/page.js
@@ -20,42 +20,42 @@ export default function ProjectListPage() {
             <h1 className="text-2xl font-bold mb-8">
                 프로젝트 목록
             </h1>
-                       <div className="border rounded-md overflow-hidden">
-                            <Table>
-                                <TableHeader>
-                                    <TableRow className="bg-gray-100">
-                                        <TableHead className="text-center border-r">
-                                            프로젝트 이름
-                                        </TableHead>
-                                        <TableHead className="text-center border-r">
-                                            설명
-                                        </TableHead>
-                                        <TableHead className="text-center">
-                                            프로젝트 멤버
-                                        </TableHead>
-                                    </TableRow>
-                                </TableHeader>
-                                <TableBody>
-                                    {teamListMock.map((project) => (
-                                        <TableRow key={project.id}>
-                                            <TableCell className="text-center border-r">
-                                                {project.name}
-                                            </TableCell>
-                                            <TableCell className="text-center border-r">
-                                                {project.description}
-                                            </TableCell>
-                                            <TableCell className="text-center flex">
-                                                {project.members.length > 0 && project.members.map((member, index) => (
-                                                    <div key={`${member.id}_${index}`} className={`w-10 h-10 text-sm ${bgColors[Math.floor(Math.random() * bgColors.length)]} text-white rounded-full flex items-center justify-center font-semibold`}>
-                                                        {getAvatarFallback(member.name)}
-                                                    </div>
-                                                ))}
-                                            </TableCell>
-                                        </TableRow>
+            <div className="border rounded-md overflow-hidden">
+                <Table>
+                    <TableHeader>
+                        <TableRow className="bg-gray-100">
+                            <TableHead className="text-center border-r">
+                                프로젝트 이름
+                            </TableHead>
+                            <TableHead className="text-center border-r">
+                                설명
+                            </TableHead>
+                            <TableHead className="text-center">
+                                프로젝트 멤버
+                            </TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {teamListMock.map((project) => (
+                            <TableRow key={project.id}>
+                                <TableCell className="text-center border-r">
+                                    {project.name}
+                                </TableCell>
+                                <TableCell className="text-center border-r">
+                                    {project.description}
+                                </TableCell>
+                                <TableCell className="text-center flex">
+                                    {project.members.length > 0 && project.members.map((member, index) => (
+                                        <div key={`${member.id}_${index}`} className={`w-10 h-10 text-sm ${bgColors[Math.floor(Math.random() * bgColors.length)]} text-white rounded-full flex items-center justify-center font-semibold`}>
+                                            {getAvatarFallback(member.name)}
+                                        </div>
                                     ))}
-                                </TableBody>
-                            </Table>
-                        </div>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </div>
         </>
     )
 }
