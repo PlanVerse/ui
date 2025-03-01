ea5e24e2900afe8a64b0b84669c646df71d6cf45
LimHaeJun <6462jun@gmail.com>
2025-03-01 21:57:51 +0900

    프로젝트 리스트 페이지 수정

--- clientpages/project/ProjectListPage.jsx
+++ clientpages/project/ProjectListPage.jsx
@@ -3,22 +3,34 @@
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { getAvatarFallback } from "@/lib/avatar";
 import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 import { getApi } from "@/lib/axios";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import { boolean } from "zod";
+import { z } from "zod";
+import { X } from "lucide-react";
+import { Textarea } from "@/components/ui/textarea";
+import { zodResolver } from "@hookform/resolvers/zod";
+import DetailModal from "@/components/DetailModal";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+// import { boolean } from "zod";
 // import { useState } from "react";
 
+const projectDetailSchema = z.object({
+    projectName: z.string().max(50),
+    projectDescription: z.string().max(100),
+})
 
-
-// Todo: 권한에 따라 프로젝트 생성버튼이 있거나 없게 하기
-
-// const invalidProjectCreator = () => {
-//     // true면 관리자 false면 권한 없음
-//     const [projectCreatorInfos, setProjectCreatorInfos ] = useState(true);
-// }
-
-const ProjectTable = ({ plist }) => (
+const ProjectTable = ({ 
+    list, 
+    setDetailModalIsOpen,
+    setSelectedProject,
+    isCreator,
+    setIsCreator,
+}) => (
     <div className="border rounded-md overflow-hidden">
         <Table>
             <TableHeader>
@@ -29,16 +41,15 @@ const ProjectTable = ({ plist }) => (
                     <TableHead className="text-center border-r">
                         설명
                     </TableHead>
-                    <TableHead className="text-center border-r">
+                    <TableHead className="text-center">
                         프로젝트 멤버
                     </TableHead>
-                    <TableHead>
-                    </TableHead>
+                    <TableHead className="w-32 text-center"></TableHead>
                 </TableRow>
             </TableHeader>
             
             <TableBody>
-                {plist.map((project) => (
+                {list.map((project) => (
                     <TableRow key={project.id}>
                         <TableCell className="text-center border-r">
                             {project.name}
@@ -47,52 +58,73 @@ const ProjectTable = ({ plist }) => (
                             {project.description}
                         </TableCell>
                         <TableCell className="text-center flex">
-                            {project.projectMemberInfos.length > 0 && project.projectMemberInfos.map((member, index) => (
-                                <div key={`${member.id}_${index}`} className={`w-10 h-10 text-sm border border-gray-500 text-white rounded-full flex items-center justify-center font-semibold`}>
-                                    {getAvatarFallback(member.name)}
+                            {project.projectMemberInfos && project.projectMemberInfos.length > 0 && project.projectMemberInfos.map((member, index) => (
+                                <div key={`${member.id}_${index}`} className={`w-10 h-10 text-sm border border-gray-400 rounded-full flex items-center justify-center font-semibold`}>
+                                    {getAvatarFallback(member.username)}
                                 </div>
                             ))}
                         </TableCell>
-                        <TableCell className="text-center flex">
-                            <Link 
-                                href="/project/detailedinformation"
-                            >
+                        <TableCell className="w-32 text-center">
                             <Button
                                 variant="outline"
-                                className="bg-primary-500"
+                                className="bg-primary-500 text-white px-3"
+                                onClick={() => {
+                                    setSelectedProject(project);
+                                    setDetailModalIsOpen(true);
+                                    setIsCreator(isCreator);
+                                }}
                             >
-                                설정
+                                상세정보
                             </Button>
-                            </Link>
                         </TableCell>
                     </TableRow>
                 ))}
             </TableBody>
         </Table>
-        <div
+        {/* <div
             className="w-full flex justify-end"
         >
             <Link href="/project/create">
                 <Button
                     variant="outline"
-                    className="bg-primary-500"
+                    className="bg-primary-500 text-white disabled:bg-gray-400"
                     // disabled={!invalidProjectCreator}
                 >
                     새 프로젝트 생성
                 </Button>
             </Link>
-        </div>
+        </div> */}
     </div>
 )
 
-export default function ProjectListPage({token}) {
+export default function ProjectListPage( { token }) {
     // 프로젝트 목록 페이지
     const [isLoading, setIsLoading] = useState(true);
-    const [createdProjectList, setCreatedProjectList] = useState([]);
-    const [joinedProjectList, setJoinedProjectList] = useState([]);
-    
+    const [projectList, setProjectList] = useState([]);
+    const [detailModalIsOpen, setDetailModalIsOpen] = useState(false);
+    const [selectedProject, setSelectedProject] = useState(null);
+    const [isCreator, setIsCreator] = useState(false);
+    const [projectMember, setProjectMember] = useState("");
+    const [projectMembers, setProjectMembers] = useState([]);
+
+    const form = useForm({
+        resolver: zodResolver(projectDetailSchema),
+        defaultValues: {
+            teamName: "",
+            teamDescription: ""
+        }
+    });
+
+    function addProjectMember() {
+        setProjectMember("");
+        if (projectMembers.includes(projectMember)) {
+            return;
+        }
+        setProjectMembers([...projectMembers, projectMember]);
+    };
+
     // async function fetchProjectInfos() {
-    //     const requestProjectInfos = await getApi(/project/info/1`,
+    //     const requestProjectInfos = await getApi(`${process.env.API_URL}/project/info/1`,
     //     {
     //         id: number,
     //         projectInfoId: number,
@@ -109,19 +141,19 @@ export default function ProjectListPage({token}) {
     // }
 
     useEffect(() => {
-    async function fetchCreatedProjectList() {
-        const requestCreatedProjectList = await getApi(`/project/list/1`, null, {
+    async function fetchProjectList() {
+        const requestProjectList = await getApi(`/project/list/1`, null, {
             headers: {
                 Authorization: `Bearer ${token}`
             }
         })
            
-        if (requestCreatedProjectList.status === 401) {
+        if (requestProjectList.status === 401) {
             await removeSession();
         }
             
-        if (requestCreatedProjectList.data.content.length > 0) {
-            setCreatedProjectList(requestCreatedProjectList.data.content);
+        if (requestProjectList.data.content.length > 0) {
+            setProjectList(requestProjectList.data.content);
         };
     };
 
@@ -142,7 +174,7 @@ export default function ProjectListPage({token}) {
     // };
     
     Promise.all([
-        fetchCreatedProjectList(),
+        fetchProjectList(),
         // fetchJoinedProjectList(),
         // fetchProjectInfos(),
     ])
@@ -151,32 +183,43 @@ export default function ProjectListPage({token}) {
         });
     }, []);
 
+    useEffect(() => {
+        if (selectedProject) {
+            setProjectMembers(selectedProject.projectMemberInfos.map((projectMember) => projectMember.email));
+        }
+    }, [selectedProject]);
+
     return(
         <>
         <h1 className="text-2xl font-bold mb-8">
             프로젝트 목록
         </h1>
-        {createdProjectList.length > 0 &&
+        {projectList.length > 0 &&
             <h2 className="text-xl font-semibold mb-8">
                 생성한 프로젝트
             </h2> &&
-            <ProjectTable list={createdProjectList} />
+            <ProjectTable 
+                list={projectList}
+                setDetailModalIsOpen={setDetailModalIsOpen}
+                setSelectedProject={setSelectedProject} 
+                isCreator={true}
+                setIsCreator={setIsCreator}
+            />
         }
-        <div className="h-px bg-gray-200 my-8"></div>
-        {joinedProjectList.length > 0 &&
+        {/* <div className="h-px bg-gray-200 my-8"></div> */}
+        {/* {joinedProjectList.length > 0 &&
             <h2 className="text-xl font-semibold mb-8">
                 소속된 프로젝트
             </h2> &&
             <ProjectTable list={joinedProjectList} />
-        }
+        } */}
         {/* Todo: 권한에 따라 메시지와 버튼유무를 다르게 하기 */}
-        {createdProjectList.length === 0 && joinedProjectList.length === 0 &&
+        {projectList.length === 0 &&
             <div className="w-full h-[calc(100vh-176px)] flex flex-col gap-4 items-center justify-center">
                 <p className="w-fit">
-                    소속된 프로젝트나 생성한 프로젝트가 없습니다.
+                    소속된 프로젝트가 없습니다.
                 </p>
                 <Link href="/project/create">
-                    { /*requestProjectInfos.creator === true && */
                     <Button
                         variant="outline"
                         className="bg-primary-500 text-white"
@@ -184,10 +227,133 @@ export default function ProjectListPage({token}) {
                     >
                         새 프로젝트 생성
                     </Button>
-                    }
                 </Link>
             </div>
         }
+            <DetailModal
+                title="프로젝트 상세정보"
+                isOpen={detailModalIsOpen}
+                setIsOpen={setDetailModalIsOpen}
+            >
+                <Form {...form}>
+                    <form
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            form.handleSubmit(onSubmit)(e);
+                        }}
+                        className="space-y-8"
+                    >
+                        <FormField
+                            control={form.control}
+                            name="projectName"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>프로젝트 이름</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            {...field}
+                                            id="projectName"
+                                            placeholder="팀명을 입력하세요"
+                                            className="rounded-sm"
+                                            value={selectedProject.name || field.value}
+                                            onChange={(e) => {
+                                                setSelectedProject({
+                                                    ...selectedProject,
+                                                    name: e.target.value
+                                                });
+                                            }}
+                                            disabled={!isCreator}
+                                            required={isCreator}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="projectDescription"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>설명</FormLabel>
+                                    <FormControl>
+                                        <Textarea
+                                            {...field}
+                                            id="projectDescription"
+                                            placeholder="팀 설명을 입력하세요"
+                                            className="rounded-sm"
+                                            value={selectedProject.description || field.value}
+                                            onChange={(e) => {
+                                                setSelectedProject({
+                                                    ...selectedProject,
+                                                    description: e.target.value
+                                                });
+                                            }}
+                                            disabled={!isCreator}
+                                        />
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                        />
+                        <div className="relative">
+                            <Label className="text-sm font-medium">팀원</Label>
+                            <Input
+                                id="projectMembers"
+                                placeholder="추가할 팀원의 이메일 주소를 입력해주세요"
+                                onChange={(e) => {
+                                    setProjectMember(e.target.value);
+                                }}
+                                value={projectMember}
+                                className="rounded-sm my-2"
+                                onKeyDown={(e) => {
+                                    if (e.key === "Enter") {
+                                        e.preventDefault();
+                                        addProjectMember();
+                                    };
+                                }}
+                                disabled={!isCreator}
+                            />
+                            {projectMember.length > 0 && (
+                                <div
+                                    className="absolute top-[69px] left-0 right-0 bg-gray-100 p-2"
+                                    onClick={addProjectMember}
+                                >
+                                    {projectMember}
+                                </div>
+                            )}
+                            <div className="w-full flex gap-2 flex-wrap">
+                                {projectMembers.length > 0 &&
+                                    projectMembers.map((projectMember, index) => (
+                                        <Badge
+                                            key={index}
+                                            variant="secondary"
+                                            className="w-fit flex items-center gap-2"
+                                        >
+                                            {projectMember}
+                                            <X
+                                                className="w-3 h-3"
+                                                onClick={() => {
+                                                    if (!isCreator) return;
+                                                    setProjectMembers(projectMembers.filter((m) => m !== projectMember));
+                                                }}
+                                            />
+                                        </Badge>
+                                    ))
+                                }
+                            </div>
+                        </div>
+                        <div className="flex justify-end">
+                            <Button
+                                type="submit"
+                                className="bg-primary-500 text-white disabled:bg-gray-400"
+                                disabled={!isCreator}
+                            >
+                                저장
+                            </Button>
+                        </div>
+                    </form>
+                </Form>
+            </DetailModal>
     </>
     )
 }
