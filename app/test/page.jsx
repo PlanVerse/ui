ebe54b43b449f1bafb3cb01216bb0b9e0997b3e6
seoeungi <sjlim02255@gmail.com>
2025-03-09 02:15:43 +0900

    에디터 임시 커밋

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useRef } from "react";
 import { getApi, postApi } from "@/lib/axios";
+import { getSession } from "@/lib/session";
 
 import EditorJS from "@editorjs/editorjs";
 import Header from "@editorjs/header";
@@ -18,7 +19,6 @@ import CodeTool from "@editorjs/code";
 import Delimiter from "@editorjs/delimiter";
 import InlineCode from "@editorjs/inline-code";
 import Table from "@editorjs/table";
-import Paragraph from "@editorjs/paragraph";
 
 export default function Page() {
   const ejInstance = useRef(null);
@@ -41,21 +41,33 @@ export default function Page() {
         image: {
           class: ImageTool,
           config: {
-            endpoints: {
-              byFile: 'http://localhost:50051/api/v1/editor/upload'
-            },
-            field: "file",
-          },
-          uploader: {
-            uploadByFile(file) {
-              return postApi(`${process.env.NEXT_PUBLIC_API_URL}/editor/upload`).then(response => {
-                return {
-                  success: 122,
-                  file: {
-                    url: response.file,
+            uploader: {
+              async uploadByFile(file) {
+                const token = await getSession();
+
+                const bodyData = await new FormData();
+                bodyData.append("file", file);
+
+                const config = {
+                  headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "multipart/form-data",
                   },
                 };
-              })
+
+                return await postApi(`${process.env.NEXT_PUBLIC_API_URL}/editor/upload/99`, bodyData, config).then(async (uploadResponse) => {
+                  const config = {
+                    headers: {
+                      Authorization: `Bearer ${token}`,
+                    },
+                  };
+
+                  const fetchResponse = await getApi(`${process.env.NEXT_PUBLIC_API_URL}/${uploadResponse.file.url}`, null, config);
+                  uploadResponse.preview = fetchResponse.file.preview;
+
+                  return uploadResponse;
+                });
+              },
             },
           },
         },
@@ -114,36 +126,30 @@ export default function Page() {
 
       },
 
-      defaultBlock: 'paragraph',
+      defaultBlock: "paragraph",
 
       placeholder: "Write something or press / to select a tool",
       autofocus: true,
 
-      // data: {
-      //   blocks: [
-      //     {
-      //       id: "zcKCF1S7X8",
-      //       type: "header",
-      //       data: {
-      //         text: "Editor.js",
-      //         level: 1,
-      //       },
-      //     },
-      //     {
-      //       id: "9802bjaAA2",
-      //       type: "image",
-      //       data: {
-      //         caption: "",
-      //         withBorder: true,
-      //         withBackground: false,
-      //         stretched: false,
-      //         file: {
-      //           url: "editor/99/a3e16d3e-e99e-4ce2-90d6-1ff4b2f6d2ed",
-      //         },
-      //       },
-      //     },
-      //   ],
-      // },
+      data: {
+        blocks: [
+          {
+            id: "9802bjaAA2",
+            type: "image",
+            data: {
+              caption: "",
+              withBorder: true,
+              withBackground: false,
+              stretched: false,
+              file: {
+                url: "editor/99/88c4a389-6685-4064-9dce-cb50477b8c25",
+                // todo 하단 객체로 랜더링 되도록 해야함
+                preview: "https://s.pstatic.net/static/www/mobile/edit/20240112_1095/upload_1705057885416AaxUM.png",
+              },
+            },
+          },
+        ],
+      },
 
       onReady: () => {
         ejInstance.current = editor;
@@ -155,7 +161,7 @@ export default function Page() {
         console.log(content);
       },
     });
-  }
+  };
 
   useEffect(() => {
     if (ejInstance.current === null) {
@@ -166,11 +172,11 @@ export default function Page() {
       ejInstance?.current?.destroy();
       ejInstance.current = null;
     };
-  }, [])
+  }, []);
 
   return (
     <>
-      <div style={{marginTop: "200px"}}>
+      <div style={{ marginTop: "200px" }}>
         <div id="editor" />
       </div>
     </>
