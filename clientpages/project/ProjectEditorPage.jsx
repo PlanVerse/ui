16d5dbb46fec5cb7cf2f24a6a34b3ac249a46628
leesh9098 <leesh9098@naver.com>
2025-03-09 20:15:48 +0900

    25.03.09.1 edit editorjs

--- clientpages/project/ProjectEditorPage.jsx
+++ clientpages/project/ProjectEditorPage.jsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { useEffect, useRef } from "react";
 import { getApi, postApi } from "@/lib/axios";
+import { getSession } from "@/lib/session";
+
 import EditorJS from "@editorjs/editorjs";
 import Header from "@editorjs/header";
 import ImageTool from "@editorjs/image";
@@ -16,17 +19,14 @@ import CodeTool from "@editorjs/code";
 import Delimiter from "@editorjs/delimiter";
 import InlineCode from "@editorjs/inline-code";
 import Table from "@editorjs/table";
-import Paragraph from "@editorjs/paragraph";
 
-import { useEffect, useRef } from "react";
-
-export default function ProjectEditorPage({ token }) {
+export default function Page() {
   const ejInstance = useRef(null);
 
   const initEditor = () => {
     const editor = new EditorJS({
       readOnly: false,
-      holder: "editorjs",
+      holder: "editor",
       inlineToolbar: ["link", "marker", "bold", "italic"],
       tools: {
         header: {
@@ -37,36 +37,52 @@ export default function ProjectEditorPage({ token }) {
           },
           shortcut: "CMD+SHIFT+H",
         },
+
         image: {
           class: ImageTool,
           config: {
-            endpoints: {
-              byFile: 'http://localhost:50051/api/v1/editor/upload'
-            },
-            field: "file",
-          },
-          uploader: {
-            uploadByFile(file) {
-              return postApi(`${process.env.NEXT_PUBLIC_API_URL}/editor/upload`).then(response => {
-                return {
-                  success: 122,
-                  file: {
-                    url: response.file,
+            uploader: {
+              async uploadByFile(file) {
+                const token = await getSession();
+
+                const bodyData = await new FormData();
+                bodyData.append("file", file);
+
+                const config = {
+                  headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "multipart/form-data",
                   },
                 };
-              })
+
+                return await postApi(`${process.env.NEXT_PUBLIC_API_URL}/editor/upload/99`, bodyData, config).then(async (uploadResponse) => {
+                  const config = {
+                    headers: {
+                      Authorization: `Bearer ${token}`,
+                    },
+                  };
+
+                  const fetchResponse = await getApi(`${process.env.NEXT_PUBLIC_API_URL}/${uploadResponse.file.url}`, null, config);
+                  uploadResponse.preview = fetchResponse.file.preview;
+
+                  return uploadResponse;
+                });
+              },
             },
           },
         },
+
         list: {
           class: NestedList,
           inlineToolbar: true,
           shortcut: "CMD+SHIFT+L",
         },
+
         checklist: {
           class: Checklist,
           inlineToolbar: true,
         },
+
         quote: {
           class: Quote,
           inlineToolbar: true,
@@ -76,62 +92,70 @@ export default function ProjectEditorPage({ token }) {
           },
           shortcut: "CMD+SHIFT+O",
         },
+
         warning: Warning,
+
         marker: {
           class: Marker,
           shortcut: "CMD+SHIFT+M",
         },
+
         code: {
           class: CodeTool,
           shortcut: "CMD+SHIFT+C",
         },
+
         delimiter: Delimiter,
+
         inlineCode: {
           class: InlineCode,
           shortcut: "CMD+SHIFT+C",
         },
+
         linkTool: LinkTool,
+
         raw: RawTool,
+
         embed: Embed,
+
         table: {
           class: Table,
           inlineToolbar: true,
           shortcut: "CMD+ALT+T",
         },
+
       },
-      defaultBlock: 'paragraph',
+
+      defaultBlock: "paragraph",
+
       placeholder: "Write something or press / to select a tool",
       autofocus: true,
-      // data: {
-      //   blocks: [
-      //     {
-      //       id: "zcKCF1S7X8",
-      //       type: "header",
-      //       data: {
-      //         text: "Editor.js",
-      //         level: 1,
-      //       },
-      //     },
-      //     {
-      //       id: "9802bjaAA2",
-      //       type: "image",
-      //       data: {
-      //         caption: "",
-      //         withBorder: true,
-      //         withBackground: false,
-      //         stretched: false,
-      //         file: {
-      //           url: "editor/99/a3e16d3e-e99e-4ce2-90d6-1ff4b2f6d2ed",
-      //         },
-      //       },
-      //     },
-      //   ],
-      // },
+
+      data: {
+        blocks: [
+          {
+            id: "9802bjaAA2",
+            type: "image",
+            data: {
+              caption: "",
+              withBorder: true,
+              withBackground: false,
+              stretched: false,
+              file: {
+                url: "editor/99/88c4a389-6685-4064-9dce-cb50477b8c25",
+                // todo 하단 객체로 랜더링 되도록 해야함
+                preview: "https://s.pstatic.net/static/www/mobile/edit/20240112_1095/upload_1705057885416AaxUM.png",
+              },
+            },
+          },
+        ],
+      },
 
       onReady: () => {
         ejInstance.current = editor;
       },
-      onChange: async (_api, _event) => {
+
+      onChange: async (api, event) => {
         let content = await editor.saver.save();
 
         console.log(content);
@@ -149,10 +173,12 @@ export default function ProjectEditorPage({ token }) {
       ejInstance.current = null;
     };
   }, []);
-  
+
   return (
-    <div>
-      <div id="editorjs" />
-    </div>
+    <>
+      <div>
+        <div id="editor" />
+      </div>
+    </>
   );
 }
